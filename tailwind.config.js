185452cdf50aa98f8ae94d61f17018828079dc63
Daniël van der Winden <d.vanderwinden@gmail.com>
2025-06-13 14:20:27 +0200

    Added Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,njk,md,js}",
+    "./_site/**/*.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
+
